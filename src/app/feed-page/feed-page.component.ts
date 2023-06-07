c12c14ca2408412a0fb48abefe67ff163b4da0bb
m4ite <maitesenaibosch@gmail.com>
2023-06-07 09:22:59 -0300

    Session e Local Storage

--- src/app/feed-page/feed-page.component.ts
+++ src/app/feed-page/feed-page.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-feed-page',
   templateUrl: './feed-page.component.html',
   styleUrls: ['./feed-page.component.css']
 })
-export class FeedPageComponent {
+export class FeedPageComponent implements OnInit
+{
+  user: string | null = null
 
+  ngOnInit(): void 
+  {
+    this.user = sessionStorage.getItem("user")
+  }
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
-selector: 'app-login-page',
-templateUrl: './login-page.component.html',
-styleUrls: ['./login-page.component.css']
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
   email = ""
-  link = ''
+  link = ""
+  password = ""
+
+  constructor(private router: Router) { }
+
+  passwordChanged(event: any)
+  {
+    this.password = event
+  }
+
+  login()
+  {
+    // Aqui precisariamos fazer essa verificação no banco de dados
+    if (this.email == "email@email.com" && this.password == "123") {
+      // Isso evidentemente não é seguro, mas a ideia é bom e será melhorada no futuro
+      sessionStorage.setItem('user', 'pamella');
+      this.router.navigate(["/feed"])
+    }
+  }
 }
